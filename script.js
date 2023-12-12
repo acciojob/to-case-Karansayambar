09594796df3920084061449b620212c4d72910ab
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-12 22:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function toCase(text) {
   // write your code here
+	if(text == " "){
+		return "-";
+	}else if(text.lowerCase()){
+		return text.uppercase
+	}
+	}else if(text.upperCase()){
+		return text.lowercase
+	}
 }
 
 // DO not change the code below
-
 const text = prompt("Enter text:");
 alert(toCase(text));
